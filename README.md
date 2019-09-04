d564f6d7d2e0f2f31564cbf3ccff59eba289c2aa
wind2esg <wind2esg@gmail.com>
2019-09-05 07:23:38 +0800

    v1.0.1 npm published

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # wgdown
-node multi-process download tool
+A download tool of node with multi-process feature.  
+
+Basic is `request` , `pipe` and `writeStream`.  
+
+However when using this async, download uncompletely problems come.
+This tool uses child_process to act async download and Retry if the file is download uncompletely.
+
+## key points
++ A queue maintains resource url and local download path.
++ Child processes consume the item in queue. Push the item back to the queue for retrying. `shift()` follows `fork()` because of async.
++ Compare `response.header["content-length"]` with `WriteStream.bytesWritten` to check whether the file is complete. if not, retry.
++ When error occurs, Retry.
++ Child processes send message to the parent process, which is checking or downloading result of the current item. On recieving message, parent process would decide retry or not by operating the queue depending on message.
++ Record the number of child processes. When the number reaches 0, it means no child processes currently, that is, download finished. callback here.
+
+## useage
+### install
+Add dependence to package.json  
+`"wgdown" : "~1.0.1"`  
+`npm install`
+
+### use
+`require('wgdown')(options);`
+
++ `options.list` An object array. The object in it contains the target resource url `object.serverPath` and your local path `object.localPath`.
++ `options.cpus` Numbers of the child processes. Commonly, it is the number of cpus.
++ `options.errorLimit` It would retry the url if times of error under `errorLimit`.
++ `options.callback` Callback when then job done.
+  + `log` It is a log for the job. `exist` How many files are already at local. `noResource` The number of files which don't exist on server. `download` The number of files downloaded. `error` How many files couldn't be downloaded because of error. `child` Stands for the number of child processes.
+  + `errorList` It is the list of `object.serverPath`, recording urls not be downloaded.
+
+### example
+```
+downloadList.push({serverPath:<resource url>, localPath:<local path>});
+
+require('wgdown')({
+      list: downloadList,
+      cpus: require('os').cpus().length,
+      errorLimit: 4,
+      callback: (log, errorList)=>{
+        console.log(log);
+        if(errorList.length != 0){
+          console.log(errorList);
+        }
+      }
+    });
+```
---
+++ wgdown.js
@@ -0,0 +1,162 @@
+const fs = require('fs');
+const childProcess = require('child_process');
+const request = require('request');
+
+// parent
+
+let wgdown = (options)=>{
+
+    let parent = (options)=>{
+        if (options.list.length == 0){
+          return ;
+        }
+    
+        let errorCount = {};
+    
+        log.child++;
+        let downloadTarget = options.list[0];
+        childProcess.fork('./node_modules/wgdown', [downloadTarget.serverPath, downloadTarget.localPath])
+                      .on('message', (message)=>{
+                        let msg = JSON.parse(message);
+                        switch(msg.result){
+                            case '0':
+                                console.log("downloaded:  " + msg.serverPath);
+                                log.download++;
+                                break;
+                            case '1':
+                                console.log("exists on local:  " + msg.serverPath);
+                                log.exist++;
+                                break;
+                            case '2':
+                                console.log("no resource on server:  " + msg.serverPath);
+                                log.noResource++;
+                                break;
+                            case '5':
+                                console.log("error :  " + msg.serverPath);
+                                errorCount[msg.serverPath] == undefined ? errorCount[msg.serverPath] = 1 : errorCount[msg.serverPath]++;
+                                if(errorCount[msg.serverPath] < options.errorLimit){
+                                    options.list.push(msg);
+                                    if(fs.existsSync(msg.localPath)){
+                                        fs.unlinkSync(msg.localPath);
+                                    }
+                                } else {
+                                    errorList.push(msg.serverPath);
+                                    log.error++;
+                                }
+                                break;
+                            case '3':
+                                console.log("problem @:  " + msg.serverPath);
+                                options.list.push(msg);
+                                if(fs.existsSync(msg.localPath)){
+                                    fs.unlinkSync(msg.localPath);
+                                }
+                                break;
+                            default:
+                                console.log("unknown child message");
+                        }
+                        parent(options);
+                    })
+                    .on('exit', ()=>{
+                        log.child--;
+                        if(log.child == 0){
+                            options.callback(log, errorList);
+                        }
+                    });
+    
+        options.list.shift();
+    }
+
+    let log = {};
+
+    log.exist = 0;
+    log.noResource = 0;
+    log.download = 0;
+    log.error = 0;
+    log.child = 0;
+    
+    let errorList = [];
+
+    for (let i = 0; i < options.cpus; i++){
+        parent(options);
+    }
+
+
+}
+
+module.exports = wgdown;
+
+// child
+
+let serverPath = process.argv[2];
+let localPath = process.argv[3];
+let size = 0;
+
+// result
+// '0' init
+// '1' local exists
+// '2' not exist in server
+// '3' download but problem
+// '0' downloaded 
+// '5' error
+
+return new Promise((resolve, reject)=>{
+    let msg = {
+        localPath: localPath,
+        serverPath: serverPath,
+        result: '0'
+    };
+
+    if(fs.existsSync(localPath)){
+        msg.result = '1';
+        process.send(JSON.stringify(msg));
+        resolve();
+        return ;
+    }
+
+    let rs = request(serverPath);
+    rs.on('error', (err)=>{
+              console.log("error:  " + serverPath + "   " + err);
+              msg.result = '5';
+              process.send(JSON.stringify(msg));
+              resolve();
+              return ;
+              })
+      .on('response', (response)=>{
+          if(response.statusCode == 200){
+          // console.log("download :  " + serverPath);
+          // console.log("content-length:  " +  response.headers["content-length"]);
+              size = response.headers["content-length"];
+              let ws = fs.createWriteStream(localPath);
+              rs.pipe(ws.on('error',(err)=>{
+                      console.log(localPath);
+                      console.log(err);
+                      ws.end();
+                      msg.result = '5';
+                      process.send(JSON.stringify(msg));
+                      resolve();
+                      return ;
+                  })
+                  .on('finish',()=>{
+                      // console.log(localPath + ' finished');
+                      if( size > ws.bytesWritten){
+                      // console.log("problem @:  "  + serverPath);
+                          ws.end();
+                          msg.result = '3';
+                          process.send(JSON.stringify(msg));
+                          resolve();
+                          return ;
+                      }
+                      ws.end();
+                      process.send(JSON.stringify(msg));     
+                      resolve();
+                      return ;
+                  })
+                )
+          }else{
+              msg.result = '2';
+              process.send(JSON.stringify(msg));
+              resolve();
+              return ;
+          }
+      })
+  });
